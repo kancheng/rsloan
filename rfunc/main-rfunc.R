94f38b19dd36aa8c3671e78c23f5c36ed74189b6
kancheng <zxdfgcv@gmail.com>
2017-04-09 16:00:36 +0800

    edit R function & Upload R File

---
+++ rfunc/main-rfunc.R
@@ -0,0 +1,20 @@
+# R Kan Dev Function IO
+source("C:/dataipt/rfunc/io.R")
+
+# R Kan Dev Function HCA
+source("C:/dataipt/rfunc/hca.R")
+
+# R Multiple graphs on one page 
+source("C:/dataipt/rfunc/mulggplotpic.R")
+
+# R Kan Dev Function AS Class & Head List
+source("C:/dataipt/rfunc/ashd.R")
+
+# R Kan Dev Function Pic Opt
+source("C:/dataipt/rfunc/picopt.R")
+
+# R Kan Dev Function HCA
+source("C:/dataipt/rfunc/hcaon.R")
+
+# R Multiple graphs build object
+source("C:/dataipt/rfunc/mulggplotpic-build.R")
