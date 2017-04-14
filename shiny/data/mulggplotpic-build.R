cb1736aa1bcf963d36157a94fb1993f8ca7abf1f
kancheng <zxdfgcv@gmail.com>
2017-04-14 22:57:01 +0800

    edit ggplot to shiny function

--- shiny/data/mulggplotpic-build.R
+++ shiny/data/mulggplotpic-build.R
@@ -1,18 +1,13 @@
 # HCA Multiple ggplot proc
 
-sg2proc = function( dft, midx, cidx, lbobj){
+sg2proc = function( dft, midx, cidx){
 
 	for( snm in 1:length(cidx)){
-		lbobj.nm = paste0(lbobj,"ldta")
-		lbobj.cmdr = paste0(lbobj, "[", lbobj , "$subj == \"", cidx[snm],"\", ]")
-		lbobj.eval = eval(parse(text = lbobj.cmdr))
-		assign( lbobj.nm, lbobj.eval )
 		s.nm = paste0( dft, ".", cidx[snm], ".", "gmp")	
 		s.cmdr = paste0( "ggplot(", dft, 
 		") + geom_point( aes(x = ", 
 		midx, ", y = ", cidx[snm], 
-		", colour = factor(hcadata) )) + facet_grid( ~ loam) + labs( title = \"", 
-		lbobj.eval$sdas, " ", lbobj.eval$subd, " ", lbobj.eval$grade, "\" ) + scale_colour_hue( l = 50 ) + ",
+		", colour = factor(hcadata) )) + facet_grid( ~ loam) + scale_colour_hue( l = 50 ) + ",
 		"geom_text( check_overlap = TRUE, angle = 45, aes( x = ", midx, " + 1, y = ", cidx[snm], " + 1, label = bnm))")
 		s.eval = eval(parse(text = s.cmdr))
 		assign( s.nm, s.eval, env = .GlobalEnv)
@@ -26,25 +21,16 @@ mg2proc = function( dft, midx, cidx, lbobj){
 	mupobjnm = vector(mode = "character", length = 0)
 	mup.head.c = "multiplot( "
 	mup.tail.c = paste0(" cols = ", mupnbid, " )")
+
 	for( snm in 1:length(cidx)){
-		lbobj.nm = paste0(lbobj,"ldta")
-		lbobj.cmdr = paste0(lbobj, "[", lbobj , "$subj == \"", cidx[snm],"\", ]")
-		lbobj.eval = eval(parse(text = lbobj.cmdr))
-		assign( lbobj.nm, lbobj.eval )
+
 		s.nm = paste0( dft, ".", cidx[snm], ".", "gmp")	
-
-
 		s.cmdr = paste0( "ggplot(", dft, 
 		") + geom_point( aes(x = ", 
 		midx, ", y = ", cidx[snm], 
-		", colour = factor(hcadata) )) + facet_grid( ~ loam) + labs( title = \"", 
-		lbobj.eval$sdas, " ", lbobj.eval$subd, " ", lbobj.eval$grade, "\" ) + scale_colour_hue( l = 50 )")
-
+		", colour = factor(hcadata) )) + facet_grid( ~ loam) + scale_colour_hue( l = 50 )")
 		mupobjnm = paste0( mupobjnm, s.nm,", ")
-
-
 		s.eval = eval(parse(text = s.cmdr))
-
 		assign(s.nm, s.eval)
 	}
 		mupw.nm = paste0( dft, ".","mulgmp")
