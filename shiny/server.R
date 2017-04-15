66426bc1970fb2fbdafc1e3cf1f8e0a54dcfd522
kancheng <zxdfgcv@gmail.com>
2017-04-15 23:25:51 +0800

    Upload edit server & ui R File

--- shiny/server.R
+++ shiny/server.R
@@ -1,36 +1,212 @@
+set.seed(929)
+
 library(shiny)
-library(ggplot2)
 library(RMySQL)
+library(ggplot2)
 
-function(input, output) {
 
+# SERVER R File & Object
+
+server = function(input, output, session) {
+  
+  # tem
+  tmsp = reactiveValues()
+  
+  hclust.methods = c("ward.D", "single", "complete", "average", "mcquitty", "median", "centroid", "ward.D2")
+  dist.methods = c("euclidean", "maximum", "manhattan", "canberra", "binary" , "minkowski")
+  
+  # source r file
+  source("./data/main-rfunc.R")
+  source("./data/demo.R")
+  
+  
+  # Instruction
+  output$swdmtb = renderTable({demo}, caption = paste("If you want to use the RSLoan, Please download demo csv file."),
+                              caption.placement = getOption("xtable.caption.placement", "top"),
+                              caption.width = getOption("xtable.caption.width", NULL)
+  )
+  
+  # DB Connect
   conn = dbConnect(MySQL( ), dbname = "rsloan", username = "root", password = "hitachi")
   
-  im08 = dbGetQuery(conn, "SELECT * FROM im08")
-  im09 = dbGetQuery(conn, "SELECT * FROM im09")
+  # DB Table Name
+  keydfn = c( "bu08", "bu09", "bu10", "bu11", "bu12", "bu13", "bu14", "bu15",
+              "fi08", "fi09", "fi10", "fi11", "fi12", "fi13", "fi14", "fi15",
+              "ib08", "ib09", "ib10", "ib11", "ib12", "ib13", "ib14", "ib15",
+              "id08", "id09", "id10", "id11", "id12", "id13", "id14", "id15",
+              "im08", "im09", "im10", "im11", "im12", "im13", "im14", "im15")
   
-  dbDisconnect(conn)
+  selesqltb = function (ind){
+    for( i in 1:length(ind)){
+      cmds = paste0("dbGetQuery(conn, \"SELECT * FROM ", ind[i], "\")")
+      assign(ind[i],
+             eval(parse(text = cmds))
+             , env = .GlobalEnv)
+      
+    }
+  }
+  
+  # all data
+  selesqltb(keydfn)
+  
+  # all table name
+  # tablename = dbGetQuery(conn, "SHOW TABLES")
+  
+  # Data Input
+  
+  output$view = renderTable({
+    indsw = switch(input$inputdt,
+                   
+                   wkupfdt = {
+                     inFile = input$upfile
+                     if (is.null(inFile)){ return(NULL) }
+                     tmsp$cudf = read.csv(inFile$datapath, header = input$header, sep = input$sep,  quote = input$quote)
+                     if ( length(colnames(tmsp$cudf)) < 20 ){
+                       head( tmsp$cudf , n = input$obsr)[1:length(colnames(tmsp$cudf))]
+                     }else{
+                       head( tmsp$cudf , n = input$obsr)[1:input$obsc]
+                     }
+                     
+                   },
+                   
+                   wkdsct = {
+                     tmsp$cudf = datasetInput()
+                     if ( length(colnames(tmsp$cudf)) < 20 ){
+                       head( tmsp$cudf , n = input$obsr)[1:length(colnames(tmsp$cudf))]
+                     }else{
+                       head( tmsp$cudf , n = input$obsr)[1:input$obsc]
+                     }
+                     
+                   }
+    )
+  })
   
   datasetInput = reactive({
     switch(input$dataset,
-	"im09" = im09,
-	"im08" = im08)
-  })
-
-  
-  
-  output$summary = renderPrint({
-    dataset = datasetInput()
-    summary(dataset)
+           # "table" = tablename,
+           "bu08" = bu08, "bu09" = bu09, "bu10" = bu10, "bu11" = bu11, "bu12" = bu12, "bu13" = bu13, "bu14" = bu14, "bu15" = bu15,
+           "fi08" = fi08, "fi09" = fi09, "fi10" = fi10, "fi11" = fi11, "fi12" = fi12, "fi13" = fi13, "fi14" = fi14, "fi15" = fi15,
+           "ib08" = ib08, "ib09" = ib09, "ib10" = ib10, "ib11" = ib11, "ib12" = ib12, "ib13" = ib13, "ib14" = ib14, "ib15" = ib15,
+           "id08" = id08, "id09" = id09, "id10" = id10, "id11" = id11, "id12" = id12, "id13" = id13, "id14" = id14, "id15" = id15,
+           "im08" = im08, "im09" = im09, "im10" = im10, "im11" = im11, "im12" = im12, "im13" = im13, "im14" = im14, "im15" = im15
+    )
   })
   
-  # Show the first "n" observations
-  output$view = renderTable({
-    head(datasetInput(), n = input$obs)
+  # Setting
+  output$alldtscolnm = renderPrint({
+    colnames(tmsp$cudf)
   })
   
-
+  output$pcselt = renderPrint({
+    tmsp$pkb = input$pchoser
+    cat(tmsp$pkb)
+  })
+  
+  output$cbcselt = renderPrint({
+    tmsp$cbase = input$cbchoser
+    cat(tmsp$cbase)
+  })
+  
+  # Cluster
+  
+  
+  output$cluplot = renderPlot({
+    hcaon(tmsp$cudf, eval(parse(text = tmsp$cbase)), eval(parse(text = tmsp$pkb)))
+  })
+  
+  output$clutable = renderTable({
+    hcaon(tmsp$cudf, eval(parse(text = tmsp$cbase)), eval(parse(text = tmsp$pkb)))
+    untavt
+  }) 
+  
+  # Summary
+  
+  output$sumytable = renderTable({
+    hcaon(tmsp$cudf, eval(parse(text = tmsp$cbase)), eval(parse(text = tmsp$pkb)))
+    untal2ndsc
+  }) 
+  
+  # Analysis
+  output$anaytable = renderTable({
+    hcaon(tmsp$cudf, eval(parse(text = tmsp$cbase)), eval(parse(text = tmsp$pkb)))
+    unttkav
+  })  
+  
+  # SLoan
+  
+  output$hsloandt = renderTable({
+    hcaon(tmsp$cudf, eval(parse(text = tmsp$cbase)), eval(parse(text = tmsp$pkb)))
+    untln1dsc
+  })
+  
+  output$nsloandt = renderTable({
+    hcaon(tmsp$cudf, eval(parse(text = tmsp$cbase)), eval(parse(text = tmsp$pkb)))
+    untln0dsc
+  })  
+  
+  # Propotion
+  output$pptndt = renderTable({
+    hcaon(tmsp$cudf, eval(parse(text = tmsp$cbase)), eval(parse(text = tmsp$pkb)))
+    untslon
+  })  
+  
+  # Diagram
+  mainindex = c( "cala" )
+  courindex1 = c( "itdc")
+  
+  courindex2 = c( "itdc", "cppg", "pcpg", "oopg", "itdcn", "cala", "calb", "ec", 
+                  "dtst", "nwkpm", "sadm", "idbs", "st", "mana", "inkpg", "dbms", "mis")
+  
+  output$spbt1vto = renderPrint({
+    tmsp$spbtxt1 = input$spbtxt1
+    cat(tmsp$spbtxt1)
+  })
+  
+  output$spbt2vto = renderPrint({
+    tmsp$spbtxt2 = input$spbtxt2
+    cat(tmsp$spbtxt2)
+  })
+  
+  output$mpb1vto = renderPrint({
+    tmsp$mpbtxt1 = input$mpbtxt1
+    cat(tmsp$mpbtxt1)
+  })
+  
+  output$mpb2vto = renderPrint({
+    tmsp$mpbtxt2 = input$mpbtxt2
+    cat(tmsp$mpbtxt2)
+  })
+  
+  
+  output$singplot = renderPlot({
+    tmsp$evalspbtxt1 = eval(parse(text = tmsp$spbtxt1))
+    tmsp$evalspbtxt2 = eval(parse(text = tmsp$spbtxt2))
+    tmsp$shspic  = sg2proc("untavt", tmsp$evalspbtxt1, tmsp$evalspbtxt2)
+    untavt.gmp
+  })
+  
+  output$multiplot = renderPlot({
+    tmsp$evalmpbtxt1 = eval(parse(text = tmsp$mpbtxt1))
+    tmsp$evalmpbtxt2 = eval(parse(text = tmsp$mpbtxt2))
+    tmsp$shmpic = mg2proc("untavt", tmsp$evalmpbtxt1, tmsp$evalmpbtxt2)
+    untavt.mulgmp
+  })
+  
+  output$alldtscolnm2 = renderPrint({
+    colnames(tmsp$cudf)
+  })
+  
+  
+  # Instruction
+  
+  output$downloadDemo = downloadHandler(
+    filename = function() { 
+      paste( "demo", '.csv', sep = '') 
+    },
+    content = function(file) {
+      write.csv(demo, file)
+    }
+  )
 }
 
-
-
+shinyApp(ui = ui, server = server)
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,28 +1,218 @@
+set.seed(929)
+
 library(shiny)
+library(RMySQL)
 library(ggplot2)
 
-fluidPage(
-  
-  titlePanel("rsloan"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("dataset", "Choose a dataset:", 
-                  choices = c( "im08", "im09")),
-      
-      numericInput("obs", "Number of observations to view:", 10),
-      
-      helpText("Note: Haoye test"),
-      
-      submitButton("Update View")
-    ),
+# UI R File & Object
 
-    mainPanel(
-      h4("Observations"),
-      tableOutput("view"),
-      
-      h4("Summary"),
-      verbatimTextOutput("summary")
+ui = shinyUI(
+  fluidPage(
+    includeCSS(path = "./www/main.css"),
+    tags$head(
+      tags$link(rel = "shortcut icon", href = "https://raw.githubusercontent.com/kancheng/rsloan/master/shiny/www/favicon.ico")
+    ),
+    navbarPage("RSLoan",
+               
+               tabPanel( "Home",
+                         
+                         div( id = "home-txtbg",
+                              div( id = "home-txtct", "RSLoan"),
+                              
+                              br(),
+                              br(),
+                              
+                              div( id = "home-micttxt",
+                                   "The system is to analyze students' learning performance and economic status.")
+                              ,br()),
+                         
+                         div( titlePanel("About"),
+                              "Organization : Lunghwa University of Science and Technology",
+                              br(),
+                              "Author : Haoye",
+                              br(),
+                              "Github : ",a("https://github.com/kancheng/rsloan",href="https://github.com/kancheng/rsloan"),
+                              br(),
+                              "ResearchGate : ",a("https://www.researchgate.net/profile/Hao_Cheng_Kan",href="https://www.researchgate.net/profile/Hao_Cheng_Kan")
+                         )
+               ),
+               tabPanel("Work",
+                        tabsetPanel("Analyze",
+                                    tabPanel("Import",
+                                             sidebarLayout(
+                                               sidebarPanel(width = 3,
+                                                            radioButtons("inputdt", "Choose :",
+                                                                         c("Sample Data" = "wkdsct","External Data" = "wkupfdt")
+                                                            ),
+                                                            numericInput("obsr", "Row View:", 15, min = 1, width = "100%"),
+                                                            numericInput("obsc", "Col View:", 20, min = 1, width = "100%"),
+                                                            submitButton("Update", icon("refresh"), width = "100%"),
+                                                            # tags$hr(),
+                                                            tags$br(),
+                                                            selectInput("dataset", "Sample Data :", 
+                                                                        choices = c(
+                                                                          "bu08", "bu09", "bu10", "bu11", "bu12", "bu13", "bu14", "bu15",
+                                                                          "fi08", "fi09", "fi10", "fi11", "fi12", "fi13", "fi14", "fi15",
+                                                                          "ib08", "ib09", "ib10", "ib11", "ib12", "ib13", "ib14", "ib15",
+                                                                          "id08", "id09", "id10", "id11", "id12", "id13", "id14", "id15",
+                                                                          "im08", "im09", "im10", "im11", "im12", "im13", "im14", "im15")),
+                                                            fileInput('upfile', 'External Data : ',
+                                                                      accept = c('text/csv', 
+                                                                                 'text/comma-separated-values,text/plain',  '.csv')),
+                                                            checkboxInput('header', 'Header', TRUE),
+                                                            radioButtons('sep', 'Separator',
+                                                                         c(Comma=',',
+                                                                           Semicolon=';',
+                                                                           Tab='\t'),
+                                                                         ','),
+                                                            radioButtons('quote', 'Quote',
+                                                                         c(None='',
+                                                                           'Double Quote'='"',
+                                                                           'Single Quote'="'"),
+                                                                         '"'),
+                                                            helpText("Please follow the instruction went Upload dataset.")),
+                                               mainPanel(width = 7,
+                                                         br(),
+                                                         h1("Observations"),
+                                                         br(),
+                                                         tableOutput("view")
+                                               )
+                                             )
+                                    ),
+                                    tabPanel("Setting",
+                                             h2( "Dataset Column : " ),
+                                             helpText("Display the Dataset's Colume name which user's choice. "),
+                                             br(),
+                                             textOutput("alldtscolnm", container = pre),
+                                             br(),
+                                             h2( "Primary Column : " ),
+                                             textInput("pchoser", "", value = "c(\"sid\")"),
+                                             br(),
+                                             verbatimTextOutput("pcselt"),
+                                             br(),
+                                             submitButton("Submit", icon("refresh"), width = "30%"),
+                                             hr(),
+                                             h2( "Cluster Base Column : " ),
+                                             textInput("cbchoser", "", value = "c(\"loam\", \"mm\" )" ),
+                                             verbatimTextOutput("cbcselt"),
+                                             br(),
+                                             submitButton("Submit", icon("refresh"), width = "30%")
+                                    ),
+                                    tabPanel("Cluster",
+                                             tabsetPanel(
+                                               tabPanel("Dataset",
+                                                        h2("Cluster :"),
+                                                        submitButton("Submit", icon("refresh"), width = "30%"),
+                                                        br(),
+                                                        tableOutput('clutable')
+                                               ),
+                                               tabPanel("Plot",
+                                                        h2("Plot :"),
+                                                        submitButton("Submit", icon("refresh"), width = "30%"),
+                                                        br(),
+                                                        plotOutput('cluplot')
+                                               )
+                                             )
+                                    ),
+                                    tabPanel("Summary",
+                                             h2("Summary :"),
+                                             submitButton("Submit", icon("refresh"), width = "30%"),
+                                             br(),
+                                             tableOutput('sumytable')
+                                    ),
+                                    tabPanel("Analysis",
+                                             h2("Analysis :"),
+                                             submitButton("Submit", icon("refresh"), width = "30%"),
+                                             br(),
+                                             tableOutput('anaytable')
+                                    ),
+                                    tabPanel("SLoan",
+                                             tabsetPanel(
+                                               tabPanel( "Have SLoan",
+                                                         h2("Have SLoan :"),
+                                                         submitButton("Submit", icon("refresh"), width = "30%"),
+                                                         br(),
+                                                         tableOutput('hsloandt')
+                                               ),
+                                               tabPanel( "Not SLoan",
+                                                         h2("Not SLoan :"),
+                                                         submitButton("Submit", icon("refresh"), width = "30%"),
+                                                         br(),
+                                                         tableOutput('nsloandt')
+                                               )
+                                             )
+                                    ),
+                                    tabPanel("Propotion",
+                                             h2("Propotion :"),
+                                             submitButton("Submit", icon("refresh"), width = "30%"),
+                                             br(),
+                                             tableOutput('pptndt')
+                                    ),
+                                    tabPanel("Diagram",
+                                             tabsetPanel(
+                                               tabPanel("Plot Setting",
+                                                        sidebarLayout(
+                                                          sidebarPanel(width = 3,
+                                                                       h1( "Dataset Column : " ),
+                                                                       helpText("Display the Dataset's Colume name which user's choice. "),
+                                                                       br(),
+                                                                       textOutput("alldtscolnm2", container = pre)
+                                                          ),
+                                                          mainPanel(width = 7,
+                                                                    h2( "1. Single Plot :" ),
+                                                                    h3("PK Column : "),
+                                                                    textInput("spbtxt1", "", value = "c(\"cala\")"),
+                                                                    verbatimTextOutput("spbt1vto"),
+                                                                    submitButton("Submit", icon("refresh"), width = "30%"),
+                                                                    h3("Analyze Column : "),
+                                                                    textInput("spbtxt2", "", value = "c(\"itdc\")"),
+                                                                    verbatimTextOutput("spbt2vto"),
+                                                                    submitButton("Submit", icon("refresh"), width = "30%"),
+                                                                    br(),
+                                                                    h2( "2. Multiple Plot :" ),
+                                                                    h3("PK Column : "),
+                                                                    textInput("mpbtxt1", "", value = "c(\"cala\")"),
+                                                                    verbatimTextOutput("mpb1vto"),
+                                                                    submitButton("Submit", icon("refresh"), width = "30%"),
+                                                                    h3("Analyze Column : "),
+                                                                    textInput("mpbtxt2", "", value = "c(\"mm\", \"ma\", \"fm\", \"ac\", \"eca\", \"ecb\", \"calb\")", width = "100%"),
+                                                                    verbatimTextOutput("mpb2vto"),
+                                                                    submitButton("Submit", icon("refresh"), width = "30%")
+                                                          )
+                                                        )
+                                               ),
+                                               tabPanel("Single Plot",
+                                                        h2("Single Plot"),
+                                                        br(),
+                                                        submitButton("Submit", icon("refresh"), width = "30%"),
+                                                        br(),
+                                                        plotOutput("singplot")
+                                               ),
+                                               tabPanel("Multiple Plot",
+                                                        h2("Multiple Plot"),
+                                                        br(),
+                                                        submitButton("Submit", icon("refresh"), width = "30%"),
+                                                        br(),
+                                                        plotOutput("multiplot")
+                                               )
+                                             )
+                                    )
+                        )
+               ),
+               
+               tabPanel("Instruction",
+                        sidebarLayout(
+                          sidebarPanel("Instruction",
+                                       downloadButton('downloadDemo', 'Download')
+                          ),
+                          mainPanel(
+                            # demo data csv
+                            tableOutput("swdmtb")
+                          )
+                        )
+               )
+               
     )
   )
 )
+
