4b2db9e499b55e28b5305c5595ce237804f9f7d6
kancheng <zxdfgcv@gmail.com>
2017-05-17 20:39:43 +0800

    Upload Traditional Chinese

--- shiny-server/rsloan/server.R
+++ shiny-server/rsloan/server.R
@@ -19,7 +19,7 @@ function(input, output, session) {
   # source r file
   source("./data/main-rfunc.R")
   source("./data/demo.R")
-  
+  source("./data/colnm.R")
   
   # Instruction
   output$swdmtb = renderTable({demo}, caption = paste("If you want to use the RSLoan, Please download demo csv file."),
@@ -36,22 +36,23 @@ function(input, output, session) {
               "ib08", "ib09", "ib10", "ib11", "ib12", "ib13", "ib14", "ib15",
               "id08", "id09", "id10", "id11", "id12", "id13", "id14", "id15",
               "im08", "im09", "im10", "im11", "im12", "im13", "im14", "im15")
-  
+
+ # Data Col Name
   selesqltb = function (ind){
     for( i in 1:length(ind)){
-      cmds = paste0("dbGetQuery(conn, \"SELECT * FROM ", ind[i], "\")")
-      assign(ind[i],
-             eval(parse(text = cmds))
-             , env = .GlobalEnv)
-      
+      cmds1 = paste0("dbGetQuery(conn, \"SELECT * FROM ", ind[i], "\")")
+      cmds2 = paste0("colnames(", ind[i],") = ", ind[i] ,"cn")
+      cmds3 = paste0(ind[i])
+      eval.cmds1 = eval(parse(text = cmds1))
+      assign(ind[i], eval.cmds1)
+      eval(parse(text = cmds2))
+      eval.cmds3 = eval(parse(text = cmds3))
+      assign(ind[i], eval.cmds3, env =.GlobalEnv)
     }
   }
-  
-  # all data
   selesqltb(keydfn)
   
-  # all table name
-  # tablename = dbGetQuery(conn, "SHOW TABLES")
+
   
   # Data Input
   
@@ -84,12 +85,17 @@ function(input, output, session) {
   
   datasetInput = reactive({
     switch(input$dataset,
-           # "table" = tablename,
-           "bu08" = bu08, "bu09" = bu09, "bu10" = bu10, "bu11" = bu11, "bu12" = bu12, "bu13" = bu13, "bu14" = bu14, "bu15" = bu15,
-           "fi08" = fi08, "fi09" = fi09, "fi10" = fi10, "fi11" = fi11, "fi12" = fi12, "fi13" = fi13, "fi14" = fi14, "fi15" = fi15,
-           "ib08" = ib08, "ib09" = ib09, "ib10" = ib10, "ib11" = ib11, "ib12" = ib12, "ib13" = ib13, "ib14" = ib14, "ib15" = ib15,
-           "id08" = id08, "id09" = id09, "id10" = id10, "id11" = id11, "id12" = id12, "id13" = id13, "id14" = id14, "id15" = id15,
-           "im08" = im08, "im09" = im09, "im10" = im10, "im11" = im11, "im12" = im12, "im13" = im13, "im14" = im14, "im15" = im15
+           "資訊管理系 - 2008" = im08, "資訊管理系 - 2009" = im09, "資訊管理系 - 2010" = im10, "資訊管理系 - 2011" = im11, "資訊管理系 - 2012" = im12, 
+           "資訊管理系 - 2013" = im13, "資訊管理系 - 2014" = im14, "資訊管理系 - 2015" = im15,
+           "企業管理系 - 2008" = bu08, "企業管理系 - 2009" = bu09, "企業管理系 - 2010" = bu10, "企業管理系 - 2011" = bu11, "企業管理系 - 2012" = bu12, 
+           "企業管理系 - 2013" = bu13, "企業管理系 - 2014" = bu14, "企業管理系 - 2015" = bu15,
+           "財務金融系 - 2008" = fi08, "財務金融系 - 2009" = fi09, "財務金融系 - 2010" = fi10, "財務金融系 - 2011" = fi11, "財務金融系 - 2012" = fi12, 
+           "財務金融系 - 2013" = fi13, "財務金融系 - 2014" = fi14, "財務金融系 - 2015" = fi15,
+           "國際企業系 - 2008" = ib08, "國際企業系 - 2009" = ib09, "國際企業系 - 2010" = ib10, "國際企業系 - 2011" = ib11, "國際企業系 - 2012" = ib12, 
+           "國際企業系 - 2013" = ib13, "國際企業系 - 2014" = ib14, "國際企業系 - 2015" = ib15,
+           "工業管理系 - 2008" = id08, "工業管理系 - 2009" = id09, "工業管理系 - 2010" = id10, "工業管理系 - 2011" = id11, "工業管理系 - 2012" = id12, 
+           "工業管理系 - 2013" = id13, "工業管理系 - 2014" = id14, "工業管理系 - 2015" = id15
+
     )
   })
   
@@ -155,26 +161,25 @@ function(input, output, session) {
   mainindex = c( "cala" )
   courindex1 = c( "itdc")
   
-  courindex2 = c( "itdc", "cppg", "pcpg", "oopg", "itdcn", "cala", "calb", "ec", 
-                  "dtst", "nwkpm", "sadm", "idbs", "st", "mana", "inkpg", "dbms", "mis")
+  courindex2 = c( "itdc", "mana", "sta", "cala")
   
   output$spbt1vto = renderPrint({
-    tmsp$spbtxt1 = input$spbtxt1
+    tmsp$spbtxt1 = paste0( "c(", input$spbtxt1, ")")
     cat(tmsp$spbtxt1)
   })
   
   output$spbt2vto = renderPrint({
-    tmsp$spbtxt2 = input$spbtxt2
+    tmsp$spbtxt2 = paste0( "c(", input$spbtxt2, ")")
     cat(tmsp$spbtxt2)
   })
   
   output$mpb1vto = renderPrint({
-    tmsp$mpbtxt1 = input$mpbtxt1
+    tmsp$mpbtxt1 = paste0( "c(", input$mpbtxt1, ")")
     cat(tmsp$mpbtxt1)
   })
   
   output$mpb2vto = renderPrint({
-    tmsp$mpbtxt2 = input$mpbtxt2
+    tmsp$mpbtxt2 = paste0( "c(", input$mpbtxt2, ")")
     cat(tmsp$mpbtxt2)
   })
   
